58e6407e2ba0797b5151fda9f959333fa1e1548a
Vadim Kutuzov <93603697+kutuzzov@users.noreply.github.com>
2025-01-25 15:34:40 +0300

    Create main.cpp
    Итераторы в конструкторах контейнеров

---
+++ Спринт 4/Тема 20-25: Итераторы -> Урок 4-10/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <set>
+#include <algorithm>
+
+using namespace std;
+
+template <typename It>
+void PrintRange(It range_begin, It range_end) {
+    for (auto it = range_begin; it != range_end; ++it) {
+        cout << *it << " "s;
+    }
+    cout << endl;
+}
+
+template <typename It>
+auto MakeVector(It begin, It end) {
+    return vector(begin, end);
+}
+
+int main() {
+    set<string> unique_langs = {"Python"s, "Java"s, "C#"s, "Ruby"s, "C++"s};
+    vector<string> langs = MakeVector(unique_langs.begin(), unique_langs.end());
+    PrintRange(langs.begin(), langs.end());
+}
