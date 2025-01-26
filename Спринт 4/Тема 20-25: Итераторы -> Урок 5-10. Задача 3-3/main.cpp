d91ba2139364070c53b249ed4280711a4bdd3c9c
Vadim Kutuzov <93603697+kutuzzov@users.noreply.github.com>
2025-01-26 14:06:01 +0300

    Create main.cpp
    Итераторы в методах контейнеров

---
+++ Спринт 4/Тема 20-25: Итераторы -> Урок 5-10. Задача 3-3/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+template <typename It>
+void PrintRange(It range_begin, It range_end) {
+    for (auto it = range_begin; it != range_end; ++it) {
+        cout << *it << " "s;
+    }
+    cout << endl;
+}
+
+template <typename Container>
+void EraseAndPrint(Container& container, int pos, int start, int finish) {
+    container.erase(container.begin() + pos);
+    PrintRange(container.begin(), container.end());
+    container.erase(container.begin() + start, container.begin() + finish);
+    PrintRange(container.begin(), container.end());
+}
+
+int main() {
+    vector<string> langs = {"Python"s, "Java"s, "C#"s, "Ruby"s, "C++"s};
+    EraseAndPrint(langs, 0, 0, 2);
+    return 0;
+}
