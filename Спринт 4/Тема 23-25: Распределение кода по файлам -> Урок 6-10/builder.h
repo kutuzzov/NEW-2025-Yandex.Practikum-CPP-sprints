cfcd13b890af49e128009a62c96aafccc093b51c
Vadim Kutuzov <93603697+kutuzzov@users.noreply.github.com>
2025-02-11 13:32:11 +0300

    Add files via upload
    Проблема двойного включения

---
+++ Спринт 4/Тема 23-25: Распределение кода по файлам -> Урок 6-10/builder.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "square_calculation.h"
+#include "wall.h"
+
+class Builder {
+public:
+    double CalcBricksNeeded(const Wall& wall) const {
+        double height = wall.GetHeight();
+        double width = wall.GetWidth();
+        return CalcSquare(width, height) * 5;
+    }
+};
---
+++ Спринт 4/Тема 23-25: Распределение кода по файлам -> Урок 6-10/carpenter.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "wall.h"
+
+class Carpenter {
+public:
+    int CalcShelves(const Wall& wall) const;
+};
