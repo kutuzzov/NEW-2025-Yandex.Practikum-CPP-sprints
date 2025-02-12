57fbaafad8c84995368dbd2afa8913ce1b4678c8
Vadim Kutuzov <93603697+kutuzzov@users.noreply.github.com>
2025-02-12 13:05:04 +0300

    Add files via upload
    Шаблоны в многофайловых проектах

---
+++ Спринт 4/Тема 23-25: Распределение кода по файлам -> Урок 8-10/accountant.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <utility>
+
+#include "square_calculation.h"
+#include "wall.h"
+
+using namespace std;
+
+class Accountant {
+public:
+    template <class BuildingObject>
+    double CalcPaintNeeded(const BuildingObject& building_object) const;
+    template <class BuildingObject>
+    double CalcBricksNeeded(const BuildingObject& building_object) const;
+};
+
+template <class BuildingObject>
+double Accountant::CalcPaintNeeded(const BuildingObject& building_object) const {
+    pair<double, double> sizes = building_object.GetSizes();
+    return CalcSquare(sizes.first, sizes.second) * 0.4;
+}
+
+template <class BuildingObject>
+double Accountant::CalcBricksNeeded(const BuildingObject& building_object) const {
+    pair<double, double> sizes = building_object.GetSizes();
+    return CalcSquare(sizes.first, sizes.second) * 5;
+}
