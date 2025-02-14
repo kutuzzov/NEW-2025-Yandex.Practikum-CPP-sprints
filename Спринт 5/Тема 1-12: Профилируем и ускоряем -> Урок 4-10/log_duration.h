b92d36328a0d1a5b1bd972df05b000215ecc6fcc
Vadim Kutuzov <93603697+kutuzzov@users.noreply.github.com>
2025-02-14 14:33:54 +0300

    Create log_duration.h
    Упрощаем логирование

---
+++ Спринт 5/Тема 1-12: Профилируем и ускоряем -> Урок 4-10/log_duration.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <chrono>
+#include <iostream>
+#include <string>
+
+class LogDuration {
+public:
+    // заменим имя типа std::chrono::steady_clock
+    // с помощью using для удобства
+    using Clock = std::chrono::steady_clock;
+
+    LogDuration(std::string name)
+        : name_(name) {
+    }
+
+    ~LogDuration() {
+        using namespace std::chrono;
+        using namespace std::literals;
+
+        const auto end_time = Clock::now();
+        const auto dur = end_time - start_time_;
+        std::cerr << name_ << ": "s << duration_cast<milliseconds>(dur).count() << " ms"s << std::endl;
+    }
+
+private:
+    const Clock::time_point start_time_ = Clock::now();
+    std::string name_;
+};
