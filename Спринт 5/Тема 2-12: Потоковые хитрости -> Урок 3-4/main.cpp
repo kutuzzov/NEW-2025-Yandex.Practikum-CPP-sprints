a6ca5b15f02995ea9ed4af0c010f89b65588d2e5
Vadim Kutuzov <93603697+kutuzzov@users.noreply.github.com>
2025-02-25 14:07:24 +0300

    Create main.cpp
    Связь потоков: в поисках оптимального ввода и вывода

---
+++ Спринт 5/Тема 2-12: Потоковые хитрости -> Урок 3-4/main.cpp
@@ -0,0 +1,35 @@
+#include "log_duration.h"
+
+#include <iostream>
+
+using namespace std;
+
+class StreamUntier {
+public:
+    // добавьте конструктор, деструктор
+    // и дополнительные поля класса при необходимости
+    StreamUntier(istream& in)
+        : stream_(in) {
+        tied_before_ = stream_.tie(nullptr);
+    }
+
+    ~StreamUntier() {
+        stream_.tie(tied_before_);
+    }
+
+private:
+    istream& stream_;
+    ostream* tied_before_;
+};
+
+int main() {
+    LOG_DURATION("\\n with tie"s);
+
+    StreamUntier guard(cin);
+    int i;
+    while (cin >> i) {
+        cout << i * i << "\n"s;
+    }
+
+    return 0;
+}
