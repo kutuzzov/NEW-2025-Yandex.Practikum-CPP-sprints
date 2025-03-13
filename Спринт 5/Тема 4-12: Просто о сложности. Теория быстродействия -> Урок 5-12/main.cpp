c63fb98bab46ceff02db104413e2be5963cc3faa
Vadim Kutuzov <93603697+kutuzzov@users.noreply.github.com>
2025-03-13 09:46:01 +0300

    Create main.cpp
    Улучшаем сложность

---
+++ Спринт 5/Тема 4-12: Просто о сложности. Теория быстродействия -> Урок 5-12/main.cpp
@@ -0,0 +1,43 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <random>
+
+using namespace std;
+
+// функция возвращает true, если векторы из одинаковых элементов
+// перепишите эту функцию, улучшив её асимптотическую сложность
+bool TestPermut(const vector<int>& v1, const vector<int>& v2) {
+    vector<int> copy_v1 = v1;
+    vector<int> copy_v2 = v2;
+    
+    sort(copy_v1.begin(), copy_v1.end());
+    sort(copy_v2.begin(), copy_v2.end());
+
+    return copy_v1 == copy_v2;
+}
+
+int main() {
+    std::mt19937 g;
+
+    int n;
+    cin >> n;
+    vector<int> v1, v2;
+    v1.reserve(n);
+    v2.reserve(n);
+
+    for (int i = 0; i < n; ++i) {
+        v1.push_back(rand());
+        v2.push_back(rand());
+    }
+
+    // оба вектора случайны, вряд ли они совпадут
+    cout << "Random vectors match? "s << flush;
+    cout << (TestPermut(v1, v2) ? "Yes"s : "No"s) << endl;
+
+    // делаем один перестановкой другого явным образом
+    v2 = v1;
+    shuffle(v2.begin(), v2.end(), g);
+    cout << "Permuted vectors match? "s << flush;
+    cout << (TestPermut(v1, v2) ? "Yes"s : "No"s) << endl;
+}
